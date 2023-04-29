345c64e63d60166e03febf3c5c8f87225b9af3d1
charl4501 <charliesumayod@gmail.com>
2023-04-29 19:08:20 +0800

    committed

--- about.php
+++ about.php
@@ -4,9 +4,65 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Us</title>
+    <title>Brewscape</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
+    <link rel="icon" href="img/Brewscape_Mug_white.png" >
+    <link rel="stylesheet" href="css/about.css">
+    
 </head>
+
 <body>
+    
+    <nav>
+    <a href="home.php"><img src="img/Brewscape_Mug_white.png" style=" width: 120px; height: 120px;"></a>
+        <!--<img src="img/Brewscape_Mug_white.png" style=" width: 120px; height: 120px;">-->
+        <ul>
+          <li> <a href="home.php">HOME</a></li>
+            <li> <a href="menucoffee.php">MENU</a></li>
+            <li> <a href="#">FIND A STORE</a></li>
+            <li> <a href="about.php">ABOUT US</a></li>       
+            <li> <a href="#">CART</a></li>
+            <button onclick="window.location.href = 'signin.html';"id="signup-btn">Sign In</button>
+
+        </ul>
+    </nav>
+
+    <div class="image-container"> 
+    <img src="Photos/about-coffee.png" id="home" style=" width: 100%; height: 500px;">
+    </div>
+
+  <div class="about">
+	<h1>The Brewscape Story</h1>
+	<p>Three close friends, Charl, Faizza and Mayjoy, had always talked about starting a business together. They had diverse backgrounds in finance, marketing, hospitality, and design, but they shared a common love for coffee and community.
+  One day, they stumbled upon a vacant space in the heart of their city and decided to take a chance and start their own coffee/cafe shop. They spent countless hours brainstorming ideas, discussing branding, menu items, and interior design.
+  Each member of the group brought their unique skills and perspectives to the table, and they worked collaboratively to create a space that was welcoming, stylish, and inclusive. They also decided to prioritize sustainability by using compostable packaging, sourcing fair-trade coffee beans, and minimizing waste.
+  Finally, after months of hard work, the coffee/cafe shop opened its doors, and the Three friends were overwhelmed by the positive response from the community. They loved seeing people come together to enjoy great coffee, delicious food, and a warm atmosphere. The coffee/cafe shop became a beloved local gathering place, and the four friends felt proud to have created something special together.
+  </p>
+  <img src="Photos/about.jpg" alt="About Us" />
+  </div>
+
+  <div class="about">
+  <h1>Our Mission</h1>
+  <p>Our mission is to create a welcoming and cozy environment where people can come together to enjoy high-quality coffee, delicious food, and friendly conversation. We strive to provide exceptional customer service and a comfortable atmosphere that encourages relaxation and productivity. Our goal is to become a beloved local establishment that adds value to the community and fosters a sense of connection and belonging.</p>
+
+  <div class="about">
+  <h1>Our Startup Location</h1>
+  <p>Our coffee shop is located in the heart of downtown, Butuan City. just a few blocks from the main square. Our address is 656 San Francisco Street Barangay Diego Silang, and we're situated in a charming old building with lots of character. You'll recognize us by our bright red awning and the smell of fresh coffee wafting out of our doors.</p>
+
+      </div>
+        <hr style="  border-top: 3px solid #c5bdbd; margin: 50px;">
+      <footer>
+        <div class="left">
+          © 2023 Brew Scape Coffee. All rights reserved.
+        </div>
+        <div class="right">
+          <a href="https://twitter.com"><i class="fab fa-twitter"></i></a>
+          <a href="https://facebook.com"><i class="fab fa-facebook"></i></a>
+          <a href="https://instagram.com"><i class="fab fa-instagram"></i></a>
+        </div>
+      </footer>
+      
+    
     
 </body>
 </html>
