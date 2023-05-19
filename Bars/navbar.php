cd6ecfbb035398e860ef51251eb846cbd59f2aa0
mayjoycillo <cillomayjoybutao@gmail.com>
2023-05-19 22:04:27 +0800

    somechanges

--- Bars/navbar.php
+++ Bars/navbar.php
@@ -1,3 +1,8 @@
+<?php
+// Assume the authentication process is successful and the user is logged in
+$loggedIn = true;
+?>
+
 <nav>
     <a href="home.php"><img src="img/Brewscape_Mug_white.png" style="width: 120px; height: 120px;"></a>
     <ul>
@@ -5,7 +10,7 @@
         <li><a href="menucoffee.php">MENU</a></li>
         <li><a href="findstore.php">FIND A STORE</a></li>
         <li><a href="about.php">ABOUT US</a></li>
-        <li><a href="#" onclick="showConfirmation();"><i class="fa-solid fa-bell-concierge fa-xs" style="color: #ffffff;"></i> DINE WITH US</a></li>
+        <li><a href="#" onclick="showConfirmation(event);"><i class="fa-solid fa-bell-concierge fa-xs" style="color: #ffffff;"></i> DINE WITH US</a></li>    
     </ul>
 </nav>
 
@@ -14,12 +19,21 @@
 </div>
 
 <script>
-function showConfirmation() {
-    var confirmationMessage = document.getElementById("confirmationMessage");
-    confirmationMessage.classList.add("show");
-    
-    if (confirm("You need to sign in before making a reservation. Click OK to proceed to the sign-in page.")) {
-      window.location.replace("../signin.php");
+    function showConfirmation(event) {
+        event.preventDefault();
+        var confirmationMessage = document.getElementById("confirmationMessage");
+        
+        // Check if the user is not already signed in
+        <?php if (!$loggedIn) { ?>
+        confirmationMessage.classList.add("show");
+        
+        if (confirm("You need to sign in before making a reservation. Click OK to proceed to the sign-in page.")) {
+            window.location.href = "signin.php";
+        }
+        <?php } else { ?>
+        // If the user is already signed in, directly redirect to the reservation page
+        window.location.href = "reservation.php";
+        return false; // Prevents further execution of the function
+        <?php } ?>
     }
-}
 </script>
